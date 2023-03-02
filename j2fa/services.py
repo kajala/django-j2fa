3516f2b2986a7e68500104efcc9ec0c2017ab199
Jani Kajala <kajala@gmail.com>
2023-03-01 22:45:28 -0600

    added comment about auth.login in auth_2fa

--- j2fa/services.py
+++ j2fa/services.py
@@ -2,7 +2,6 @@ import logging
 from j2fa.models import TwoFactorSession
 from django.conf import settings
 from ipware.ip import get_client_ip  # type: ignore  # pytype: disable=import-error
-from django.contrib import auth
 from django.http import HttpRequest
 
 logger = logging.getLogger(__name__)
@@ -11,13 +10,12 @@ logger = logging.getLogger(__name__)
 def auth_2fa(request: HttpRequest, user, reason: str = ""):
     """
     By-pass 2FA requirement for this user session.
+    Call auth.login() before this function if the user is not logged in yet.
     :param request:
     :param user:
     :param reason: Optional reason for by-passing 2FA, e.g. "bank ID". Just stored in log.
     :return:
     """
-    if not user.is_authenticated:
-        auth.login(request, user)
     ip = get_client_ip(request)[0]
     if ip is None and settings.DEBUG:
         ip = "127.0.0.1"
