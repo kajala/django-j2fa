692190530ee106a8215f1df34bb1882850bb2741
Jani Kajala <kajala@gmail.com>
2024-06-18 14:43:47 +0700

    Release 3.8.10

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-j2fa",
-    version="3.8.9",
+    version="3.8.10",
     author="Jani Kajala",
     author_email="kajala@gmail.com",
     packages=find_packages(exclude=["project", "venv"]),
